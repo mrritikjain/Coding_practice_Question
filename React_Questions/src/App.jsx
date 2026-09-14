7893f3a1465872b8f1a9ae45a7d480d52567a749
RITIK JAIN <72255272+mrritikjain@users.noreply.github.com>
2026-09-14 22:24:25 +0530

    Day1 Q3 added

---
+++ React_Questions/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Greetings from "./Components/Greetings";
+
+const App = (name) => {
+  return (
+    <div>
+      <Greetings  />
+    </div>
+  );
+};
+
+export default App;
---
+++ React_Questions/src/Components/Greetings.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Greetings = ({ name }) => {
+  return (
+    <div>
+      <h2>Hello {name || "Guest !"}</h2>
+    </div>
+  );
+};
+
+export default Greetings;
